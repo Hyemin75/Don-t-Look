b0ee3235e170ba53c963f0d6a02e5bfb5d384a16
Hyemin <103083770+Hyemin75@users.noreply.github.com>
2022-08-23 16:58:53 +0900

    Add LeverBody Use()

--- Assets/Flooded_Grounds/Scripts/Player/ActionController.cs
+++ Assets/Flooded_Grounds/Scripts/Player/ActionController.cs
@@ -18,17 +18,28 @@ public class ActionController : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI actionText;
+    [SerializeField]
+    private TextMeshProUGUI useText;
 
     [SerializeField]
     private Inventory inventory;
-
-    public AudioClip getItemSound;
+    
+    [SerializeField]
     AudioSource audioSource;
+    [SerializeField]
+    public AudioClip getItemSound;
+    public AudioClip openSound;
+    public AudioClip failSound;
+    public AudioClip PullLeverSound;
+
+
+    private bool GetLever = false;
     
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         actionText.gameObject.SetActive(false);
+        useText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -40,9 +51,17 @@ public class ActionController : MonoBehaviour
     private void TryAction()
     {
         CheckItem();
+
         if(Input.GetKeyDown(KeyCode.E))
         {
-            CanPickUp();
+            if(hitInfo.transform.tag == "LeverBody")
+            {
+                CanUse();
+            }
+            else
+            {
+                CanPickUp();
+            }
         }
     }
 
@@ -57,7 +76,32 @@ public class ActionController : MonoBehaviour
                 inventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item);
                 PlaySound("GETITEM");
             }
+
+            if(hitInfo.transform.GetComponent<ItemPickUp>().name == "Lever")
+            {
+                GetLever = true;
+            }
         }
+    }
+
+    private void CanUse()
+    {
+        if (pickupActivated)
+        {
+            if(GetLever)
+            {
+                //레버 내려감 + effect 사라짐 + 문열림 애니메이션 작동
+                PlaySound("OPEN");
+                PlaySound("PULLLEVER");
+                InfoDisappear();
+            }
+            else
+            {
+                PlaySound("FAIL");
+                useText.gameObject.SetActive(true);
+            }
+        }
+
     }
 
 
@@ -65,7 +109,7 @@ public class ActionController : MonoBehaviour
     {
         if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInfo, range, layerMask))
         {
-            if(hitInfo.transform.tag == "Item")
+            if(hitInfo.transform.tag == "Item" || hitInfo.transform.tag == "LeverBody")
             {
                 ItemInfoAppear();
             }
@@ -78,15 +122,25 @@ public class ActionController : MonoBehaviour
 
     private void ItemInfoAppear()
     {
-        pickupActivated = true;
-        actionText.gameObject.SetActive(true);
-        actionText.text = hitInfo.transform.GetComponent<ItemPickUp>().item.itemName + "<color=yellow>" + "(E)" + "</color>";
+        if(hitInfo.transform.tag == "LeverBody")
+        {
+            pickupActivated = true;
+            actionText.gameObject.SetActive(true);
+            actionText.text = hitInfo.transform.GetComponent<ItemPickUp>().item.itemName + "<color=yellow>" + "(E)" + "</color>";
+        }
+        else
+        {
+            pickupActivated = true;
+            actionText.gameObject.SetActive(true);
+            actionText.text = hitInfo.transform.GetComponent<ItemPickUp>().item.itemName + "<color=yellow>" + "(E)" + "</color>";
+        }
     }
 
     private void InfoDisappear()
     {
         pickupActivated = false;
         actionText.gameObject.SetActive(false);
+        useText.gameObject.SetActive(false);
     }
 
     void PlaySound(string action)
@@ -95,6 +149,15 @@ public class ActionController : MonoBehaviour
         {
             case "GETITEM":
                 audioSource.clip = getItemSound;
+                break;
+            case "FAIL":
+                audioSource.clip = failSound;
+                break;
+            case "OPEN":
+                audioSource.clip = openSound;
+                break;
+            case "PULLLEVER":
+                audioSource.clip = PullLeverSound;
                 break;
         }
         audioSource.Play();
