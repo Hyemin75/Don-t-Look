5d7c5cd5e6e10575528467de6b96d6e4418003f1
Hyemin <103083770+Hyemin75@users.noreply.github.com>
2022-08-26 20:01:05 +0900

    Fix Code with professer

--- Assets/Flooded_Grounds/Scripts/Enemy/Enemy.cs
+++ Assets/Flooded_Grounds/Scripts/Enemy/Enemy.cs
@@ -16,28 +16,29 @@ public class Enemy : MonoBehaviour
     public EnemyState state;
     public EnemyState prevState = EnemyState.None;
 
-    NavMeshAgent agent;
-
     public GameObject target;
     Vector3 targetPos;
 
     bool isFindEnemy = false;
 
-    bool isPlayerSeeing = false;
+    bool isPlayerSeeing;
+
+    NavMeshAgent navMeshAgent;
 
     [SerializeField]
     AudioClip lookingSound;
     [SerializeField]
     AudioClip moveSound;
 
-
     [SerializeField]
     AudioSource audioSource;
-    
+
+    [SerializeField]
+    IsSeeing isSeeing;
 
     private void Awake()
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
     }
 
 
@@ -70,9 +71,10 @@ public class Enemy : MonoBehaviour
 
     void UpdateStop()
     {
-        //agent.Stop();
+        isPlayerSeeing = isSeeing.isPlayerSeeing;
         transform.GetChild(0).gameObject.tag = "Figure";
-        
+        navMeshAgent.enabled = false;
+
         //플레이어가 쳐다보는지
         if (!isPlayerSeeing)
         {
@@ -82,26 +84,32 @@ public class Enemy : MonoBehaviour
     
     void UpdateMove()
     {
-        
-        if(!isPlayerSeeing)
-        { 
-            transform.GetChild(0).gameObject.tag = "Enemy";
-
-            //거리가 멀면 속도 20f, 거리가 가까우면 7f
-            if(Vector3.Distance(target.transform.position, gameObject.transform.position) > 100f)
-            {
-                agent.speed = 20f;
-            }
-            else
-            {
-                agent.speed = 8.5f;
-            }
-            agent.SetDestination(target.transform.position);
-        }
-        else
+        if (isSeeing.isPlayerSeeing)
         {
+            audioSource.Stop();
             ChangeState(EnemyState.Stop);
+
+            return;
         }
+
+        if (100f > Vector3.Distance(target.transform.position, transform.position))
+        {
+
+            navMeshAgent.speed = 8.5f;
+        }
+        navMeshAgent.SetDestination(target.transform.position);
+        
+        //if (!isPlayerSeeing)
+        //{ 
+        //    transform.GetChild(0).gameObject.tag = "Enemy";
+
+        //    //성능에 안좋음
+            
+        //}
+        //else if(isPlayerSeeing)
+        //{
+        //    ChangeState(EnemyState.Stop);
+        //}
 
         //transform.GetChild(0).gameObject의 콜라이더에 player 닿으면 웃음소리 재생 
         
@@ -116,7 +124,11 @@ public class Enemy : MonoBehaviour
 
     void ChangeState(EnemyState nextState)
     {
-        if (prevState == nextState) return;
+        Debug.Log("체인지 함수 들어옴");
+
+        if (state == nextState)
+            return;
+        Debug.Log("리턴 안됨");
 
         StopAllCoroutines();
 
@@ -126,10 +138,22 @@ public class Enemy : MonoBehaviour
         switch (state)
         {
             case EnemyState.None: StartCoroutine(CoroutineNone()); break;
-            case EnemyState.Stop: StartCoroutine(CoroutineStop()); break;
-            case EnemyState.Move: StartCoroutine(CoroutineMove()); break;
+            case EnemyState.Stop:
+
+                StartCoroutine(CoroutineStop()); break;
+            case EnemyState.Move:
+                Debug.Log("추적 상태 시작");
+                audioSource.loop = true;
+                PlaySound("MOVETOENEMY");
+                targetPos = target.transform.position;
+
+                navMeshAgent.enabled = true;
+                navMeshAgent.speed = 20f;
+                //StartCoroutine(CoroutineMove()); break;
+                break;
             case EnemyState.Attack: StartCoroutine(CoroutineAttack()); break;
         }
+
     }
 
     #region CoroutineDetail
@@ -148,7 +172,6 @@ public class Enemy : MonoBehaviour
     {
         // 한번만 수행해야 하는 동작 (상태가 바뀔 때 마다)
         Debug.Log("멈춰있음");
-
         while (true)
         {
             yield return new WaitForSeconds(1f);
@@ -158,17 +181,12 @@ public class Enemy : MonoBehaviour
     IEnumerator CoroutineMove()
     {
         // 한번만 수행해야 하는 동작 (상태가 바뀔 때 마다)
-        Debug.Log("추적 상태 시작");
-        audioSource.loop = true;
-        PlaySound("MOVETOENEMY");
-
-        targetPos = target.transform.position;
+       
 
         while (true)
         {
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(1f);
             // 시간마다 수행해야 하는 동작 (상태가 바뀔 때 마다)
-
         }
     }
     IEnumerator CoroutineAttack()
@@ -179,10 +197,6 @@ public class Enemy : MonoBehaviour
         yield break;
     }
     #endregion
-
-
-
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -197,14 +211,11 @@ public class Enemy : MonoBehaviour
     }
 
 
-
-
-
     void PlaySound(string action)
     {
         switch (action)
         {
-            case "LOOKENMMY":
+            case "LOOKENEMY":
                 audioSource.clip = lookingSound;
                 break;
             case "MOVETOENEMY":
